be57fdbd25f42b4d0ac4f63bf83ae1b1ea5c7839
Jens Piegsa <piegsa@gmail.com>
2019-10-02 12:58:47 +0200

    solved fizz buzz kata

--- src/main/java/test/FizzBuzz.java
+++ src/main/java/test/FizzBuzz.java
@@ -3,8 +3,15 @@ package test;
 public class FizzBuzz {
 
 	public String run(final int i) {
-		if (i == 3) {
+		if (i % 3 == 0 && i % 5 == 0) {
+//		if (i % 15 == 0) {
+			return "FizzBuzz";
+		}
+		if (i % 3 == 0) {
 			return "Fizz";
+		}
+		if (i % 5 == 0) {
+			return "Buzz";
 		}
 		return String.valueOf(i);
 	}
--- src/test/java/test/FizzBuzzTest.java
+++ src/test/java/test/FizzBuzzTest.java
@@ -14,28 +14,13 @@ public class FizzBuzzTest {
 
 	final FizzBuzz fizzBuzz = new FizzBuzz();
 
-	@Test @DisplayName("returns '1' for 1.")
-	void returns1For1() {
-		final String result = fizzBuzz.run(1);
-		assertThat(result).isEqualTo("1");
-	}
-
-	@Test @DisplayName("returns '2' for 2.")
-	void returns2For2() {
-		final String result = fizzBuzz.run(2);
-		assertThat(result).isEqualTo("2");
-	}
-
-	@Test @DisplayName("returns 'Fizz' for 3.")
-	void returnsFizzFor3() {
-		final String result = fizzBuzz.run(3);
-		assertThat(result).isEqualTo("Fizz");
-	}
-
 	@DisplayName("returns expected result for given input.")
 	@ParameterizedTest(name = "returns {1} for {0}.")
-	@CsvSource({"1,1", "2,2", "3,Fizz"})
+	@CsvSource({"1,1", "2,2", "3,Fizz", "5,Buzz", "6,Fizz", "10,Buzz", "15,FizzBuzz"})
 	void test(int i, String result) {
 		assertThat(fizzBuzz.run(i)).isEqualTo(result);
 	}
+
+	// TODO try PBT
+
 }
