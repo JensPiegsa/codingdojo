362f6803437d54b98cd6162ab1e8e006c642986e
Lars Erdmann <lars.erdmann@uni-greifswald.de>
2020-01-24 13:07:42 +0100

    update gilded rose with more tests

--- src/test/java/gildedrose/GildedRoseTest.java
+++ src/test/java/gildedrose/GildedRoseTest.java
@@ -1,6 +1,7 @@
 package gildedrose;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
@@ -16,11 +17,6 @@ class GildedRoseTest {
     GildedRose gildedRose;
 
     /*
-    - Once the sell by date has passed, Quality degrades twice as fast
-	- The Quality of an item is never negative
-	- "Aged Brie" actually increases in Quality the older it gets
-	- The Quality of an item is never more than 50
-	- "Sulfuras", being a legendary item, never has to be sold or decreases in Quality
 	- "Backstage passes", like aged brie, increases in Quality as its SellIn value approaches;
 	Quality increases by 2 when there are 10 days or less and by 3 when there are 5 days or less but
 	Quality drops to 0 after the concert
@@ -29,6 +25,83 @@ class GildedRoseTest {
 
 	- "Conjured" items degrade in Quality twice as fast as normal items
      */
+    // TODO items with invalid initial quality (> 50)
+
+
+    @Test @DisplayName("\"Sulfuras\", being a legendary item, never has to be sold or decreases in Quality")
+    void sulfurasBeingALegendaryItemNeverHasToBeSoldOrDecreasesInQuality() {
+        // given
+        Item legendary = new Item("Sulfuras, Hand of Ragnaros", 1, 80);
+
+        gildedRose = new GildedRose(new Item[] {legendary});
+
+        // when
+        gildedRose.updateQualitiesAndSellIns();
+
+        // then
+        int actualPostQualityOfBrie = legendary.quality;
+        int actualSellIn = legendary.sellIn;
+        assertThat(actualPostQualityOfBrie).isEqualTo(80);
+        assertThat(actualSellIn).isEqualTo(1);
+    }
+
+    @ParameterizedTest @DisplayName("The Quality of an item is never more than 50")
+    @MethodSource("maxQualityItems")
+    void theQualityOfAnItemIsNeverMoreThan50(Item item) {
+        // given
+        gildedRose = new GildedRose(new Item[] {item});
+
+        // when
+        gildedRose.updateQualitiesAndSellIns();
+
+        // then
+        assertThat(item.quality).isLessThanOrEqualTo(50);
+    }
+
+    private static Stream<Arguments> maxQualityItems() {
+        return Stream.of(
+                arguments(new Item("Aged Brie", 1, 50)),
+                arguments(new Item("Backstage passes to a TAFKAL80ETC concert", 1, 50))
+        );
+    }
+
+    @Test @DisplayName("\"Aged Brie\" actually increases in Quality the older it gets")
+    void agedBrieActuallyIncreasesInQualityTheOlderItGets() {
+
+        for (int initialQuality = 0; initialQuality < 50; initialQuality++) {
+            // given
+            Item agedBrie = new Item("Aged Brie", 1, initialQuality);
+            gildedRose = new GildedRose(new Item[] {agedBrie});
+
+            // when
+            gildedRose.updateQualitiesAndSellIns();
+
+            // then
+            int actualPostQuality = agedBrie.quality;
+            assertThat(actualPostQuality).isGreaterThan(initialQuality);
+        }
+    }
+
+    @ParameterizedTest @DisplayName("The Quality of an item is never negative")
+    @MethodSource("positiveQualityItems")
+    void theQualityOfAnItemIsNeverNegative(Item item, int expectedQuality) {
+        // given
+        gildedRose = new GildedRose(new Item[] {item});
+
+        // when
+        gildedRose.updateQualitiesAndSellIns();
+
+        // then
+        assertThat(item.quality).isEqualTo(expectedQuality);
+    }
+
+    private static Stream<Arguments> positiveQualityItems() {
+        return Stream.of(
+                arguments(new Item("border case 0", 10, 0), 0),
+                arguments(new Item("border case 1", 0, 0), 0),
+                arguments(new Item("border case 2", 0, 1), 0)
+        );
+    }
 
     @ParameterizedTest
     @MethodSource("normalItems")
@@ -78,9 +151,7 @@ class GildedRoseTest {
     private static Stream<Arguments> normalItemsWithZeroSellIn() {
         return Stream.of(
                 arguments(new Item("Elixir of the Mongoose", 0, 7), 5),
-                arguments(new Item("+5 Dexterity Vest", 0, 20), 18),
-                arguments(new Item("border case 1", 0, 0), 0),
-                arguments(new Item("border case 2", 0, 1), 0)
+                arguments(new Item("+5 Dexterity Vest", 0, 20), 18)
         );
     }
 }
