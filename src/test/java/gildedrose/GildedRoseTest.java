723cba66da717d8fde1b5e9a4528f6d371929cd0
Jens Piegsa <piegsa@gmail.com>
2020-01-31 13:34:03 +0100

    comments removed

--- src/test/java/gildedrose/GildedRoseTest.java
+++ src/test/java/gildedrose/GildedRoseTest.java
@@ -15,8 +15,6 @@ class GildedRoseTest {
 
     GildedRose gildedRose;
 
-    // TODO items with invalid initial quality (> 50)
-
     @Test
     @DisplayName("\"Conjured\" items degrade in Quality twice as fast as normal items")
     void nm2() {
@@ -154,13 +152,7 @@ class GildedRoseTest {
 
     private static Stream<Item> normalItems() {
         return Stream.of(
-//                new Item("Aged Brie", 2, 1), //
-                new Item("Elixir of the Mongoose", 5, 7), //
-//                new Item("Sulfuras, Hand of Ragnaros", 0, 80), //
-//                new Item("Sulfuras, Hand of Ragnaros", -1, 80),
-//                new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
-//                new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49),
-//                new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49),
+                new Item("Elixir of the Mongoose", 5, 7),
                 new Item("+5 Dexterity Vest", 10, 20)
         );
     }
