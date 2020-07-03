de72f5424af6d112e954b99ba6b9375e8907757b
Jens Piegsa <piegsa@gmail.com>
2020-07-03 12:46:19 +0200

    some more tests

--- src/test/java/isaprime/PrimeTest.java
+++ src/test/java/isaprime/PrimeTest.java
@@ -2,18 +2,39 @@ package isaprime;
 
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.assertFalse;
-import org.junit.Test;
+
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
 public class PrimeTest {
-	
+
 	@DisplayName("return false for Integers smaller than one")
 	@ParameterizedTest(name = "return false for {0}")
-	@CsvSource({"0","-1","-2","-3","-4","-5"})
-	public void testIntSmallerThanOne(int i){
+	@CsvSource({"0", "-1", "-2", "-3", "-4", "-5"})
+	public void testIntSmallerThanOne(int i) {
 		assertFalse(Prime.isPrime(i));
 	}
+	
+	@Test @DisplayName("test int two")
+	void testIntTwo() {
+		assertTrue(Prime.isPrime(2));
+	}
+
+	@DisplayName("return true for known primes")
+	@ParameterizedTest(name = "return true for {0}")
+	@CsvSource({"3"})
+	public void testPrime(int i) {
+		assertTrue(Prime.isPrime(i));
+	}
+
+	@DisplayName("return false for known non-primes")
+	@ParameterizedTest(name = "return false for {0}")
+	@CsvSource({"2", "4"})
+	public void testNonPrime(int i) {
+		assertFalse(Prime.isPrime(i));
+	}
+
 
 }
