49690a8d80dbbdc35ba926991f0e623118f6ce3b
Christian <Christian.Aust@fh-stralsund.de>
2020-07-03 12:35:55 +0200

    IsAPrimeNumber Version 1.1

--- src/main/java/isaprime/Prime.java
+++ src/main/java/isaprime/Prime.java
@@ -2,6 +2,6 @@ package isaprime;
 
 public class Prime {
   public static boolean isPrime(int num) {
-    return false; //TODO
+    return false;
   }
 }
--- src/test/java/isaprime/PrimeTest.java
+++ src/test/java/isaprime/PrimeTest.java
@@ -3,8 +3,17 @@ package isaprime;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.assertFalse;
 import org.junit.Test;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 public class PrimeTest {
+	@DisplayName("return false for Integers smaller than one")
+	@ParameterizedTest(name = "return false for {0}")
+	@CsvSource({"0","-1","-2","-3","-4","-5"})
+	public void testIntSmallerThanOne(int i){
+		assertFalse(Prime.isPrime(i));
+	}
 
 	@Test
 	public void testBasic() {
