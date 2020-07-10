826cce1bd89f525b8c7c592e53eaad0c0006a3fe
larserdmann <lars.erdmann@mail.de>
2020-07-10 12:38:20 +0200

    update

--- src/test/java/bankocr/BankOcr.java
+++ src/test/java/bankocr/BankOcr.java
@@ -1,10 +1,21 @@
 package bankocr;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
 
 public class BankOcr {
 	public List<String> parse(final Path path) {
+		try (final BufferedReader bufferedReader = Files.newBufferedReader(path)){
+			final String firstLine = bufferedReader.readLine();
+			final String secondLine = bufferedReader.readLine();
+			final String thirdLine = bufferedReader.readLine();
+
+		} catch (IOException e) {
+			throw new IllegalArgumentException();
+		}
 		return null;
 	}
 }
