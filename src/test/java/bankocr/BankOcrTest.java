564d0925a7ca7bbb6916be43ac3a220468517819
larserdmann <lars.erdmann@mail.de>
2020-07-10 12:21:53 +0200

    update

---
+++ src/test/java/bankocr/BankOcrTest.java
@@ -0,0 +1,18 @@
+package bankocr;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class BankOcrTest {
+
+	@Test @DisplayName("readNumbers")
+	void readNumbers() {
+		// given
+
+
+		// when
+
+		// then
+
+	}
+}
