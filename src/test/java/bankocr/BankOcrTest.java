44b9ee58e973ddd37557a4c5cc2626e62e87897d
larserdmann <lars.erdmann@mail.de>
2020-08-07 12:27:35 +0200

    update

--- src/test/java/bankocr/BankOcrTest.java
+++ src/test/java/bankocr/BankOcrTest.java
@@ -3,15 +3,19 @@ package bankocr;
 import static org.assertj.core.api.BDDAssertions.then;
 
 import java.net.URISyntaxException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
 
 public class BankOcrTest {
 
 	private final BankOcr bankOcr = new BankOcr();
+	
+	@TempDir Path logDirectory;
 
 	@Test @DisplayName("readNumbers")
 	void readNumbers() {
@@ -19,8 +23,6 @@ public class BankOcrTest {
 		final List<AccountNumber> accountNumbers = bankOcr.parse(path);
 		then(accountNumbers).extracting(AccountNumber::toString).containsExactly("123456789","023456789");
 	}
-
-	
 
 	private Path getTestResource(final String filename) {
 		try {
@@ -30,4 +32,14 @@ public class BankOcrTest {
 		}
 	}
 
+	@Test @DisplayName("write account numbers with comment to logfile")
+	void writeAccountNumbersWithCommentToLogfile() {
+		// given
+		final Path path = getTestResource("ValidAndInvalidAccountNr.txt");
+
+		// when
+		final List<AccountNumber> accountNumbers = bankOcr.parse(path);
+		
+		// then
+	}
 }
