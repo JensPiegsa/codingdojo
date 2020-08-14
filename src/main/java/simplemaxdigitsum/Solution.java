92a420c62d5e601f2d56ac5ee7c6b2b422ec5a65
Christian <Christian.Aust@fh-stralsund.de>
2020-08-14 12:59:24 +0200

    Commit #5

--- src/main/java/simplemaxdigitsum/Solution.java
+++ src/main/java/simplemaxdigitsum/Solution.java
@@ -3,29 +3,29 @@ package simplemaxdigitsum;
 class Solution {
 
     public static long solve(final long n) {
-        if (n == 1) {
+    	long largestDigitSum=0;
+    	for(long i=n;i>=0;i--){
+    		if(digitSum(i)>largestDigitSum) {
+				largestDigitSum = digitSum(i);
+			}
+		}
+    	return largestDigitSum;
+        /*if (n == 1) {
             return 1;
         } else if (n == 2) {
             return 2;
         } else {
             return 9;
-        }
+        }*/
     }
 
 
 	public static long digitSum(final long n) {
 		String characters = String.valueOf(n);
-		int sum = 0;
+		long sum = 0;
 		for (int i = 0; i < characters.length(); i++) {
 			sum += Integer.parseInt("" + characters.charAt(i));
 		}
 		return sum;
-//		if (n == 10L) {
-//			return 1L;
-//		} else if (n == 22L) {
-//			return 4L;
-//		} else {
-//			return 8L;
-//		} 
 	}
 }
