16729aa6c3b4f3f661fd8e33b16892a9e1000511
larserdmann <lars.erdmann@mail.de>
2020-09-25 13:58:08 +0200

    commit

--- src/main/java/bankocr/AccountNumber.java
+++ src/main/java/bankocr/AccountNumber.java
@@ -1,5 +1,8 @@
 package bankocr;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @SuppressWarnings("StringConcatenationInLoop")
 public class AccountNumber {
 
@@ -22,13 +25,24 @@ public class AccountNumber {
 
     private String parseNextAccountNumber(final String... lines) {
 
+        List<List<Integer>> combinations = new ArrayList<>();
         String accountNr = "";
         for (int pos = 0; pos < NUMBER_OF_DIGITS; pos++) {
             final Digit actualDigit = parseColumnAsDigit(pos, lines);
-            final int anInt = actualDigit.getInt();
-            accountNr += anInt == -1 ? "?" : anInt;
+            final List<Integer> anInt = actualDigit.getPossibleValues();
+            combinations.add(anInt);
+
+
+            accountNr += anInt.isEmpty() ? "?" : anInt;
         }
+        
+        List<String> possibleAccountNumbers = permutate(combinations);
+//        calculateChecksum(anAccountNumber);
         return accountNr;
+    }
+
+    List<String> permutate(final List<List<Integer>> combinations) {
+        return null;
     }
 
     public boolean isValid() {
@@ -40,12 +54,12 @@ public class AccountNumber {
     }
 
     boolean isChecksumValid() {
-        int checksum = calculateChecksum();
+        int checksum = calculateChecksum(this.accountNumberString);
         final boolean isValid = checksum % 11 == 0;
         return isValid;
     }
 
-    private int calculateChecksum() {
+    private int calculateChecksum(final String accountNumberString) {
         int checksum = 0;
         for (int pos = NUMBER_OF_DIGITS - 1; pos >= 0; pos--) {
             final char digitChar = accountNumberString.charAt(pos);
