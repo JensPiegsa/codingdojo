d9a57acfef7a88a08115fae76c71e0056ac74b0b
larserdmann <lars.erdmann@mail.de>
2020-12-04 12:39:30 +0100

    medicine clash kata start

---
+++ src/main/java/medicineclash/Patient.java
@@ -0,0 +1,25 @@
+package medicineclash;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Date;
+
+public class Patient {
+
+    private Collection<Medicine> medicines = new ArrayList<Medicine>();
+
+    public void addMedicine(Medicine medicine) {
+        this.medicines.add(medicine);
+    }
+
+    public Collection<Date> clash(Collection<String> medicineNames) {
+        return clash(medicineNames, 90);
+    }
+
+    public Collection<Date> clash(Collection<String> medicineNames, int daysBack) {
+        // TODO: implement this method
+        return null;
+    }
+
+
+}
