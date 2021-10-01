61dd5a12aee868cfb7d8ca009f3afd1e3f42f610
Jens Piegsa <piegsa@gmail.com>
2021-10-01 13:00:17 +0200

    commit

--- src/test/java/datamunging/TableProcessorTest.java
+++ src/test/java/datamunging/TableProcessorTest.java
@@ -13,8 +13,21 @@ class TableProcessorTest {
 	void test() {
 		// given
 		final String soccerTableDatContent = contentOf(getClass().getResource("football.dat"));
-		Table soccer = new TableImporter().importData(soccerTableDatContent);
+		final ColumnBounds columnBounds = ColumnBounds
+				.defineBounds(0, 0, 6)
+				.and(1, 7, 22)
+				.and(2,23,28)
+				.and(3, 29, 32)
+				.and(4,33, 34)
+				.and(5,35, 42)
+				.and(6, 43, 46)
+				.and(7, 47, 49)
+				.and(8, 50, 55)
+				.and(9, 56, 58);
+		final Table soccer = new TableImporter().importData(soccerTableDatContent, columnBounds);
+		
 		// when
+		String teamName = soccer.findMinimum(6, 8, 1);
 
 		// then
 
