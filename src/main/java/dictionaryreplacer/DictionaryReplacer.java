4fc1de57857b87490e08927250bc077ff047d51c
Jens Piegsa <piegsa@gmail.com>
2022-01-07 13:07:48 +0100

    "mob next [ci-skip]"

--- src/main/java/dictionaryreplacer/DictionaryReplacer.java
+++ src/main/java/dictionaryreplacer/DictionaryReplacer.java
@@ -4,6 +4,6 @@ import java.util.Map;
 
 public class DictionaryReplacer {
 	public static String replace(final String input, final Map<String, String> dictionary) {
-		return null;
+		return "";
 	}
 }
