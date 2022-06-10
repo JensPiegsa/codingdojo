b03a1fae173319ed4f6dc221531aec6682ed0ed5
Lars Erdmann <lars.erdmann@uni-greifswald.de>
2022-06-10 13:42:57 +0200

    kata confusion
    Co-authored-by: Jens Piegsa <piegsa@gmail.com>

--- src/main/java/supermarketqueue/Solution.java
+++ src/main/java/supermarketqueue/Solution.java
@@ -1,10 +1,33 @@
 package supermarketqueue;
 
 import java.util.Arrays;
+
 public class Solution {
 
-    public static int solveSuperMarketQueue(int[] customers, int n) {
-      return 0;
+    public static int solveSuperMarketQueue(final int[] customerTimesInMinutes, final int numberOfTills) {
+        if (customerTimesInMinutes.length > 0) {
+            int[] totalMinutes = new int[numberOfTills];
+
+            for (final int customerMinutes : customerTimesInMinutes) {
+                int index = findMinIndex(totalMinutes);
+                totalMinutes[index] += customerMinutes;
+            }
+            return Arrays.stream(totalMinutes).max().getAsInt();
+        }
+        return 0;
     }
-    
+
+    private static int findMinIndex(final int[] totalMinutes) {
+        int minimum = Integer.MAX_VALUE;
+        int minimumIndex = -1;
+
+        for (int i = 0; i < totalMinutes.length; i++) {
+            if (totalMinutes[i] <= minimum) {
+                minimum = totalMinutes[i];
+                minimumIndex = i;
+            }
+        }
+        return minimumIndex;
+    }
+
 }
