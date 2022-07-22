c7dbb30d01d4436a007598c3e8dd4141344b9bfb
Jens Piegsa <piegsa@gmail.com>
2022-07-22 12:43:16 +0200

    old kata

--- src/main/java/funwithtrees/Solution.java
+++ src/main/java/funwithtrees/Solution.java
@@ -1,10 +1,25 @@
 package funwithtrees;
 
-import javax.swing.tree.TreeNode;
-
 public class Solution {
     
     static TreeNode arrayToTree(int[] array) {
-        return null; // TODO: implementation
+        if (array.length == 0) {
+            return null;
+        }
+        final TreeNode root = new TreeNode(array[0]);
+        final TreeNode currentParent = root;
+
+        final TreeNode leftChild = null;
+        final TreeNode rightChild = null;
+
+        // every even node (except 0) is a right child
+        // every odd node is a left child
+        for (int i = array.length - 1; i >= 0; i--) {
+            final int n = array[i];
+            final TreeNode treeNode = new TreeNode(i, leftChild, rightChild);
+
+        }
+
+        return root;
     }
 }
