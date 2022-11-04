c594943647a9161ea31f268c67291b3ea41bea5d
Jens Piegsa <piegsa@gmail.com>
2022-11-04 14:56:24 +0100

    kata

--- src/main/java/binomialexpansion/KataSolution.java
+++ src/main/java/binomialexpansion/KataSolution.java
@@ -1,9 +1,77 @@
 package binomialexpansion;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class KataSolution {
 
-	public static String expand(String expr) {
+	public static String expand(final String expression) {
 
-		return "";
+		final Pattern pattern = Pattern.compile(
+				"\\("         // (
+				+ "(-?\\d*)"        // a
+				+ "(\\w)"           // x
+				+ "\\+?"            // +
+				+ "(-?\\d*)"        // b
+				+ "\\)\\^"          // )^
+				+ "(\\d+)");        // n
+
+		final Matcher matcher = pattern.matcher(expression);
+
+		matcher.find();
+		
+		final String aString = matcher.group(1);
+		final String x = matcher.group(2);
+		final long a = aString.isEmpty() || "-".equals(aString) ? Long.parseLong(aString + "1") : Long.parseLong(aString);
+		final long b = Long.parseLong(matcher.group(3));
+		final long n = Long.parseLong(matcher.group(4));
+
+		System.out.println("%s <=> ( %d * %s + (%d) ) ^ %d".formatted(expression, a, x, b, n));
+
+		String result = "";
+		if (b == 0L) {
+			final long c = power(a, n);
+			if (c == -1L) {
+				result += "-";
+			} else if (c != 1L) {
+				result += c;
+			} 
+			return result + x + "^" + n;
+		}
+		for (long k = 0L; k <= n; k++) {
+			final long c = binomialCoefficient(n, k) * power(a, n - k) * power(b, k);
+			String cStr = "";
+			if (c == 1L && n - k > 0L) {
+				cStr = "";
+			} else if (c == -1L && n - k > 0L) {
+				cStr = "-";
+			} else {
+				cStr = String.valueOf(c);
+			} 
+			result += k == 0L || c < 0L ? cStr : "+" + cStr;
+
+			if (n - k == 1L) {
+				result += x;
+			} else if (n - k > 1L) {
+				result += x + "^" + (n-k);
+			}
+		}
+		return result;
+	}
+
+	static long power(final long a, final long b) {
+		return (long) Math.pow(a, b);
+	}
+
+	static long binomialCoefficient(final long n, final long k) {
+		return faculty(n) / (faculty(n - k) * faculty(k));
+	}
+
+	static long faculty(final long k) {
+		long product = 1L;
+		for (long i = 2L; i <= k; i++) {
+			product *= i;
+		}
+		return product;
 	}
 }
--- src/test/java/binomialexpansion/BinomialExpansionTest.java
+++ src/test/java/binomialexpansion/BinomialExpansionTest.java
@@ -1,7 +1,12 @@
 package binomialexpansion;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.BDDAssertions.then;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 /**
  * <a href="https://www.codewars.com/kata/540d0fdd3b6532e5c3000b5b/">https://www.codewars.com/kata/540d0fdd3b6532e5c3000b5b/</a>
@@ -10,6 +15,30 @@ public class BinomialExpansionTest {
 	
 	@Test @DisplayName("test")
 	void test() {
-		
+		then(KataSolution.expand("(x+1)^2")).isEqualTo("x^2+2x+1");
+	}
+	
+	@Test @DisplayName("test2")
+	void test2() {
+		then(KataSolution.expand("(p-1)^3")).isEqualTo("p^3-3p^2+3p-1");
+	}
+	
+	@Test @DisplayName("test3")
+	void test3() {
+		then(KataSolution.expand("(r+0)^203")).isEqualTo("r^203");
+		then(KataSolution.expand("(5r+0)^3")).isEqualTo("125r^3");
+	}
+	
+	@ParameterizedTest
+	@DisplayName("test faculty")
+	@CsvSource({"0,1", "1, 1", "2, 2", "3, 6", "4, 24", "5, 120"})
+	void testFaculty(long k, long expected) {
+		assertThat(KataSolution.faculty(k)).isEqualTo(expected);
+	}
+	
+
+	@Test @DisplayName("binomialCoefficient")
+	void binomialCoefficient() {
+		assertThat(KataSolution.binomialCoefficient(3, 2)).isEqualTo(6L / (1L * 2L));
 	}
 }
