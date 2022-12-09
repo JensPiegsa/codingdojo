70fc2714324d18e29c16b75b9b9822c0c57755e9
Jens Piegsa <piegsa@gmail.com>
2022-12-09 13:22:00 +0100

    kata

---
+++ src/main/java/rectanglesunion/RectanglesUnion.java
@@ -0,0 +1,7 @@
+package rectanglesunion;
+
+public class RectanglesUnion {
+    public static int calculateSpace(int[][] rectangles) {
+        return 0;
+    }
+}
---
+++ src/test/java/rectanglesunion/RectanglesUnionTest.java
@@ -0,0 +1,33 @@
+package rectanglesunion;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@DisplayName("A RectanglesUnion")
+
+public class RectanglesUnionTest {
+
+    @Nested @DisplayName("unit")
+    class Unit {
+    	
+    }
+    
+    @Nested @DisplayName("acceptance")
+    class Acceptance {
+
+        @Test
+        public void testZeroRectangles() {
+            int[][] recs = {};
+            assertEquals(0, RectanglesUnion.calculateSpace(recs), "Zero rectangles");
+        }
+
+        @Test
+        public void testOneRectangle() {
+            int[][] recs = {{0, 4, 11, 6}};
+            assertEquals(22, RectanglesUnion.calculateSpace(recs), "One rectangle [0, 4, 11, 6] => 22");
+        }
+    }
+}
