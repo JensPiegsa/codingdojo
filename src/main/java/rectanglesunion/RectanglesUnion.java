46fd232e8b2df1dcf6c6edeed94677524c96d735
Jens Piegsa <piegsa@gmail.com>
2022-12-16 13:59:56 +0100

    describe the changes here
    Co-authored-by: Lars Erdmann <lars.erdmann@uni-greifswald.de>

--- src/main/java/rectanglesunion/RectanglesUnion.java
+++ src/main/java/rectanglesunion/RectanglesUnion.java
@@ -1,11 +1,56 @@
 package rectanglesunion;
 
+import java.util.List;
+
 public class RectanglesUnion {
-    public static int calculateSpace(int[][] rectangles) {
+    public static int calculateSpace(final int[][] rectangles) {
         int area = 0;
-        for (int[] rectangle : rectangles) {
+        for (final int[] rectangle : rectangles) {
             area += (rectangle[2] - rectangle[0]) * (rectangle[3] - rectangle[1]);
         }
         return area;
     }
+
+    record Rectangle (int x1, int y1, int x2, int y2) {
+
+
+        public boolean isInside(final Rectangle outerRectangle) {
+            return x1 >= outerRectangle.x1 && x2 <= outerRectangle.x2
+                    && y1 >= outerRectangle.y1 && y2 <= outerRectangle.y2;
+        }
+
+        public int getArea() {
+            return Math.abs(x2 - x1) * Math.abs(y2 - y1);
+        }
+
+        public int getUnionArea(final Rectangle other) {
+            if (this.isInside(other)) {
+                return other.getArea();
+            }
+            if (other.isInside(this)) {
+                return this.getArea();
+            }
+            return -1;
+        }
+
+        public boolean isDisjunction(final Rectangle other) {
+            return false;
+        }
+
+        public boolean isInside(final Point p) {
+            return x1 < p.x && p.x < x2
+                    && y1 < p.y && p.y < y2;
+        }
+
+        public List<Point> getPoints() {
+            return List.of(
+                    new Point(x1, y1),
+                    new Point(x1, y2),
+                    new Point(x2, y1),
+                    new Point(x2, y2)
+            );
+        }
+    }
+    
+    record Point(int x, int y) {}
 }
--- src/test/java/rectanglesunion/RectanglesUnionTest.java
+++ src/test/java/rectanglesunion/RectanglesUnionTest.java
@@ -3,24 +3,92 @@ package rectanglesunion;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import rectanglesunion.RectanglesUnion.Point;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static rectanglesunion.RectanglesUnion.Rectangle;
 
 @DisplayName("A RectanglesUnion")
-
 public class RectanglesUnionTest {
 
     @Nested @DisplayName("Unit")
     class Unit {
-    	@Test @DisplayName("two intersecting rectangles case 1a")
-    	void twoIntersectingRectanglesCase1A() {
-    		// given
-    		
-    		// when
-    		
-    		// then
-    		
+
+        @Test @DisplayName("can check inside")
+        void canCheckInside() {
+        	// given
+        	final Rectangle outerRectangle = new Rectangle(0, 4, 11, 7);
+        	final Rectangle innerRectangle = new Rectangle(1, 5, 10, 6);
+
+        	// then
+        	assertThat(innerRectangle.isInside(outerRectangle)).isTrue();
+        	assertThat(outerRectangle.isInside(innerRectangle)).isFalse();
+        }        
+        
+        @Test @DisplayName("can check inside same")
+        void canCheckInsideSame() {
+        	// given
+        	final Rectangle rectangle = new Rectangle(1, 5, 10, 6);
+
+        	// then
+        	assertThat(rectangle.isInside(rectangle)).isTrue();
+        }
+
+        @Test @DisplayName("can calculate area")
+        void canCalculateArea() {
+            final Rectangle rectangle = new Rectangle(1, 5, 10, 6);
+
+            assertThat(rectangle.getArea()).isEqualTo(9);
+
+        }
+
+    	@Test @DisplayName("two intersecting rectangles case 2a and 2b")
+    	void twoIntersectingRectanglesCase2AAnd2B() {
+
+            final Rectangle outerRectangle = new Rectangle(0, 4, 11, 7);
+            final Rectangle innerRectangle = new Rectangle(1, 5, 10, 6);
+            assertThat(outerRectangle.getArea()).isEqualTo(33);
+            assertThat(innerRectangle.getArea()).isEqualTo(9);
+            assertThat(innerRectangle.getUnionArea(outerRectangle)).isEqualTo(33);
+            assertThat(outerRectangle.getUnionArea(innerRectangle)).isEqualTo(33);
     	}
+
+        @Test @DisplayName("two rectangles are disjunct")
+        void twoRectanglesAreDisjunct() {
+            final Rectangle rectangle1 = new Rectangle(0, 4, 11, 7);
+            final Rectangle rectangle2 = new Rectangle(21, 15, 30, 16);
+
+            assertThat(rectangle1.isDisjunction(rectangle2)).isTrue();
+        }
+
+        @Test @DisplayName("has point inside")
+        void hasPointInside() {
+            final Rectangle rectangle = new Rectangle(0, 4, 11, 7);
+
+            assertThat(rectangle.isInside(new Point(5, 5))).isTrue();
+            assertThat(rectangle.isInside(new Point(15, 5))).isFalse();
+
+        }
+        @Test @DisplayName("two intersecting rectangles which overlap in first quadrant")
+        void twoIntersectingRectanglesWhichOverlapInFirstQuadrant() {
+        	// given
+
+        	// when
+
+        	// then
+
+        }
+        
+        @Test @DisplayName("returns all four points")
+        void returnsAllFourPoints() {
+            final Rectangle rectangle = new Rectangle(0, 4, 11, 7);
+        	assertThat(rectangle.getPoints()).containsExactlyInAnyOrder(
+                    new Point(0, 4),
+                    new Point(0, 7),
+                    new Point(11, 7),
+                    new Point(11, 4));
+        }
     }
     
     @Nested @DisplayName("acceptance")
@@ -28,13 +96,13 @@ public class RectanglesUnionTest {
 
         @Test
         public void testZeroRectangles() {
-            int[][] recs = {};
+            final int[][] recs = {};
             assertEquals(0, RectanglesUnion.calculateSpace(recs), "Zero rectangles");
         }
 
         @Test
         public void testOneRectangle() {
-            int[][] recs = {{0, 4, 11, 6}};
+            final int[][] recs = {{0, 4, 11, 6}};
             assertEquals(22, RectanglesUnion.calculateSpace(recs), "One rectangle [0, 4, 11, 6] => 22");
         }
         
