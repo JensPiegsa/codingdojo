6d38c8331858c8f4014c721ae00a8ffb93d186e0
Lars Erdmann <lars.erdmann@uni-greifswald.de>
2023-01-06 13:43:31 +0100

    describe the changes here
    Co-authored-by: Jens Piegsa <piegsa@gmail.com>

--- src/main/java/binaryleveltree/Kata.java
+++ src/main/java/binaryleveltree/Kata.java
@@ -1,11 +1,24 @@
 package binaryleveltree;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
 
 class Kata {
-  public static List<Integer> treeByLevels(Node node)
-  {
-    //off ya go!
-    return null;
-  }
+    public static List<Integer> treeByLevels(final Node node) {
+
+        final List<Integer> values = new ArrayList<>();
+        final Queue<Node> visitLater = new LinkedList<>();
+        visitLater.add(node);
+        while (!visitLater.isEmpty()) {
+            final Node current = visitLater.poll();
+            if (current != null) {
+                values.add(current.value);
+                visitLater.add(current.left);
+                visitLater.add(current.right);
+            }
+        }
+        return values;
+    }
 }
