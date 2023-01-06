27bc74c75da3515787e565c1536d23452f3daeed
Lars Erdmann <lars.erdmann@uni-greifswald.de>
2023-01-06 13:43:31 +0100

    describe the changes here
    Co-authored-by: Jens Piegsa <piegsa@gmail.com>

--- src/test/java/binaryleveltree/SolutionTest.java
+++ src/test/java/binaryleveltree/SolutionTest.java
@@ -1,13 +1,84 @@
 package binaryleveltree;
 
+import org.assertj.core.api.BDDAssertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 import java.util.*;
-import org.junit.Test;
-import static org.junit.Assert.assertEquals;
-import org.junit.runners.JUnit4;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class SolutionTest {
     
     // Kata: https://www.codewars.com/kata/52bef5e3588c56132c0003bc
+
+    @Test @DisplayName("returns empty list for null.")
+    void returnsEmptyListForNull() {
+        assertThat(Kata.treeByLevels(null)).isEmpty();
+    }
+
+    @Test @DisplayName("returns value of single node.")
+    void returnsValueOfSingleNode() {
+        assertThat(Kata.treeByLevels(new Node(null, null, 1))).containsExactly(1);
+    }
+
+
+    @Test @DisplayName("returns [1, 2] for two nodes.")
+    void returns12ForTwoNodes() {
+        assertThat(Kata.treeByLevels(new Node(new Node(null, null, 2), null, 1))).containsExactly(1, 2);
+    }
+    
+    @Test @DisplayName("returns [1, 2, 3, 4] for four nodes.")
+    void returns1234ForFourNodes() {
+        //       1
+        //      2 3
+        //     4
+        final Node n4 = new Node(null, null, 4);
+        final Node n2 = new Node(n4, null, 2);
+        final Node n3 = new Node(null, null, 3);
+        final Node n1 = new Node(n2, n3, 1);
+        assertThat(Kata.treeByLevels(n1)).containsExactly(1, 2, 3, 4);
+    	
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     
     @Test
     public void nullTest() {
