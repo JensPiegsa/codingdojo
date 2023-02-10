7346324c7d5b26234b59efb589bfdd03ec85936d
Lars Erdmann <lars.erdmann@uni-greifswald.de>
2023-02-10 13:46:54 +0100

    describe the changes here
    Co-authored-by: Jens Piegsa <piegsa@gmail.com>

--- src/main/java/piglatin/PigLatin.java
+++ src/main/java/piglatin/PigLatin.java
@@ -1,8 +1,43 @@
 package piglatin;
 
 public class PigLatin {
-    public static String pigIt(String str) {
-        // Write code here
-        return null;
+    
+    public static String pigIt(final String input) {
+        
+        if (input.isBlank()) {
+            return input;
+        }
+
+        Character firstLetterOfWord = null;
+        String result = "";
+        boolean lastCharacterNonLetter = true;
+
+        for (int i = 0; i < input.length(); i++) {
+            final char character = input.charAt(i);
+            final boolean isLetter = Character.isLetter(character);
+
+            // detection of first letter of word
+            if (lastCharacterNonLetter && isLetter) {
+                firstLetterOfWord = character;
+            }
+
+            // one character after end of word
+            if (!isLetter && firstLetterOfWord != null) {
+                result += firstLetterOfWord + "ay";
+                firstLetterOfWord = null;
+            }
+
+            // non letter or inside word (except first letter)
+            if (!isLetter || !lastCharacterNonLetter) {
+                result += character;
+            }
+            lastCharacterNonLetter = !isLetter;
+        }
+
+        if (firstLetterOfWord != null) {
+            result += firstLetterOfWord + "ay";
+        }
+
+        return result;
     }
 }
--- src/test/java/piglatin/PigLatinTest.java
+++ src/test/java/piglatin/PigLatinTest.java
@@ -1,14 +1,59 @@
 package piglatin;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class PigLatinTest {
+    
+    // "nullay nullay!"
+
+    @Test @DisplayName("empty input")
+    void emptyInput() {
+    	assertThat(PigLatin.pigIt("")).isEqualTo("");
+    }
+    
+    @Test @DisplayName("space")
+    void space() {
+    	assertThat(PigLatin.pigIt(" ")).isEqualTo(" ");
+    }
+
+    @Test @DisplayName("simple case")
+    void simpleCase() {
+    	assertThat(PigLatin.pigIt(" !")).isEqualTo(" !");
+    }
+
+    @Test @DisplayName("punctuation")
+    void punctuation() {
+    	assertThat(PigLatin.pigIt(",!,")).isEqualTo(",!,");
+    }
+    
+    @Test @DisplayName("single uppercase word")
+    void singleUppercaseWord() {
+        assertThat(PigLatin.pigIt("Hello")).isEqualTo("elloHay");
+    }
+
+    @Test @DisplayName("two words")
+    void twoWords() {
+        assertEquals("elloHay orldway", PigLatin.pigIt("Hello world"));
+    }
+
+    @Test @DisplayName("two words with two spaces")
+    void twoWords2() {
+        assertEquals("elloHay  orldway", PigLatin.pigIt("Hello  world"));
+    }
+
+    @Test @DisplayName("two words with two spaces and leading space")
+    void twoWords3() {
+        assertEquals(" elloHay  orldway", PigLatin.pigIt(" Hello  world"));
+    }
 
     @Test
     public void FixedTests() {
         assertEquals("igPay atinlay siay oolcay", PigLatin.pigIt("Pig latin is cool"));
         assertEquals("hisTay siay ymay tringsay", PigLatin.pigIt("This is my string"));
+        assertEquals("elloHay orldway !", PigLatin.pigIt("Hello world !"));
     }
 }
