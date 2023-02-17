ac82ab3695af9ecddcb11701c9387dd97cf8b378
Lars Erdmann <lars.erdmann@uni-greifswald.de>
2023-02-17 12:44:57 +0100

    new kata
    Co-authored-by: Jens Piegsa <piegsa@gmail.com>

---
+++ src/main/java/stripcomments/StripComments.java
@@ -0,0 +1,9 @@
+package stripcomments;
+
+public class StripComments {
+
+	public static String stripComments(String text, String[] commentSymbols) {
+		return "";
+	}
+	
+}
---
+++ src/test/java/stripcomments/StripCommentsTest.java
@@ -0,0 +1,23 @@
+package stripcomments;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class StripCommentsTest {
+
+	@Test
+	public void stripComments() throws Exception {
+		assertEquals(
+				"apples, pears\ngrapes\nbananas",
+				StripComments.stripComments( "apples, pears # and bananas\ngrapes\nbananas !apples", new String[] { "#", "!" } )
+		);
+
+		assertEquals(
+				"a\nc\nd",
+				StripComments.stripComments( "a #b\nc\nd $e f g", new String[] { "#", "$" } )
+		);
+
+	}
+
+}
