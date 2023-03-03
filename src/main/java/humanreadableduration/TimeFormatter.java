e79b8fc8623b4a93d9a107c447d913e535f86b16
Jens Piegsa <piegsa@gmail.com>
2023-03-03 13:48:06 +0100

    describe the changes here
    Co-authored-by: Lars Erdmann <lars.erdmann@uni-greifswald.de>

--- src/main/java/humanreadableduration/TimeFormatter.java
+++ src/main/java/humanreadableduration/TimeFormatter.java
@@ -1,8 +1,59 @@
 package humanreadableduration;
 
+import static java.util.stream.Collectors.*;
+
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class TimeFormatter {
     
-    public static String formatDuration(int seconds) {
-        // your code goes here
+    public static String formatDuration(final int seconds) {
+        
+        final Duration duration = Duration.ofSeconds(seconds);
+        final long daysPart = duration.toDaysPart();
+        final int hourPart = duration.toHoursPart();
+        final int minutesPart = duration.toMinutesPart();
+        final int secondsPart = duration.toSecondsPart();
+        
+        final List<String> parts = new ArrayList<>();
+
+        if (seconds == 0) {
+            return "now";
+        }
+
+        if (daysPart > 0L) {
+            parts.add(daysPart + " " + pluralize(daysPart, "day"));
+        }
+        
+        if (hourPart > 0) {
+            parts.add(hourPart + " " + pluralize(hourPart, "hour"));
+        }
+        
+        if (minutesPart > 0) {
+            parts.add(minutesPart + " " + pluralize(minutesPart, "minute"));
+        }
+        
+        if (secondsPart > 0) {
+            parts.add(secondsPart + " " + pluralize(secondsPart, "second"));
+        }
+        return combine(parts);
+    }
+
+    private static String pluralize(final long amount, String unit) {
+        if (amount > 1L) {
+            unit += "s";
+        }
+        return unit;
+    }
+
+    public static String combine(final List<String> elements) {
+        final int length = elements.size();
+        return length == 1
+                ? elements.get(0)
+                : elements.stream()
+                .limit(length - 1L)
+                .collect(joining(", ")) + " and " + elements.get(length - 1);
     }
 }
--- src/test/java/humanreadableduration/TimeFormatterTest.java
+++ src/test/java/humanreadableduration/TimeFormatterTest.java
@@ -1,15 +1,79 @@
 package humanreadableduration;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.List;
+
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 class TimeFormatterTest {
 
+    // https://www.codewars.com/kata/52742f58faf5485cae000b9a/train/java
+
+    @Test @DisplayName("returns 'now' for 0.")
+    void test() {
+        assertThat(TimeFormatter.formatDuration(0)).isEqualTo("now");
+    }
+    
+    @Test @DisplayName("returns '1 second' for 1.")
+    void returns1SecondFor1() {
+        assertThat(TimeFormatter.formatDuration(1)).isEqualTo("1 second");
+    }
+
+    @Test @DisplayName("returns '2 seconds' for 2.")
+    void returns2SecondsFor2() {
+        assertThat(TimeFormatter.formatDuration(2)).isEqualTo("2 seconds");
+    }
+
+    @Test @DisplayName("returns '1 minute' for 60.")
+    void returns1MinuteFor60() {
+        assertThat(TimeFormatter.formatDuration(60)).isEqualTo("1 minute");
+    }
+
+    @Test @DisplayName("returns '1 hour' for 3600.")
+    void returns1HourFor3600() {
+        assertThat(TimeFormatter.formatDuration(3600)).isEqualTo("1 hour");
+    }
+
+    @Test @DisplayName("returns '1 day' for 86400.")
+    void returns1DayFor86400() {
+        assertThat(TimeFormatter.formatDuration(86400)).isEqualTo("1 day");
+    }
+
+    @Test @DisplayName("returns '1 minute and 1 second' for 61.")
+    void returns1MinuteAnd1SecondFor61() {
+        assertThat(TimeFormatter.formatDuration(61)).isEqualTo("1 minute and 1 second");
+    }
+    
+    @Nested @DisplayName("combinations")
+    class Combinations {
+    	
+        @Test @DisplayName("combine() returns 'a' for list of (a).")
+        void test1() {
+            assertThat(TimeFormatter.combine(List.of("a"))).isEqualTo("a");
+        }
+        
+        @Test @DisplayName("combine() returns 'a and b' for list of (a, b).")
+        void test2() {
+            assertThat(TimeFormatter.combine(List.of("a", "b"))).isEqualTo("a and b");
+        }        
+        
+        @Test @DisplayName("combine() returns 'a, b and c' for list of (a, b, c).")
+        void test3() {
+            assertThat(TimeFormatter.combine(List.of("a", "b", "c"))).isEqualTo("a, b and c");
+        }
+        
+        @Test @DisplayName("combine() returns 'a, b, c and d' for list of (a, b, c, d).")
+        void test4() {
+            assertThat(TimeFormatter.combine(List.of("a", "b", "c", "d"))).isEqualTo("a, b, c and d");
+        }
+    }
 
     @Nested
-    class AcceptanceTest{
+    class AcceptanceTest {
 
         @Test
         public void exampleTests() {
