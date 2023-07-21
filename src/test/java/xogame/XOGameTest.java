a718ec1e92a9e36ad0c84323eb51b112711551ab
Lars Erdmann <lars.erdmann@uni-greifswald.de>
2023-07-21 12:01:16 +0200

    stuff
    Co-authored-by: Jens Piegsa <piegsa@gmail.com>

---
+++ src/test/java/xogame/XOGameTest.java
@@ -0,0 +1,26 @@
+package xogame;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+/**
+ * https://kata-log.rocks/tic-tac-toe-kata
+ *
+ * 🟪❌⭕
+ *
+ * Rules
+ * In random order
+ *
+ * a game is over when all fields in a row are taken by a player
+ * players take turns taking fields until the game is over
+ * a game is over when all fields in a diagonal are taken by a player
+ * a game is over when all fields are taken
+ * there are two players in the game (X and O)
+ * a game has nine fields in a 3x3 grid
+ * a game is over when all fields in a column are taken by a player
+ * a player can take a field if not already taken
+ */
+
+public class XOGameTest {
+    
+}
