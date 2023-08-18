0b68616a56e42d94e5f3806aee779992b1a0b677
Lars Erdmann <lars.erdmann@uni-greifswald.de>
2023-08-18 12:09:01 +0200

    silent medicine clash start
    Co-authored-by: Jens Piegsa <piegsa@gmail.com>

---
+++ src/main/java/medicineclash2/Prescription.java
@@ -0,0 +1,15 @@
+package medicineclash2;
+
+import java.util.Date;
+
+public class Prescription {
+    
+    private Date dispenseDate = new Date();
+    private int daysSupply = 30;
+    
+    public Prescription(Date dispenseDate, int daysSupply) {
+        this.dispenseDate = dispenseDate;
+        this.daysSupply = daysSupply;
+    }
+
+}
