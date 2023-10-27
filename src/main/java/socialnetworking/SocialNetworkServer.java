d1e19d61b52f46330644c73f56d9fb560506007f
Jens Piegsa <piegsa@gmail.com>
2023-10-27 12:52:11 +0200

    changes
    Co-authored-by: Jacob <jacob.braeutigam@uni-greifswald.de>

--- src/main/java/socialnetworking/SocialNetworkServer.java
+++ src/main/java/socialnetworking/SocialNetworkServer.java
@@ -7,6 +7,7 @@ import java.net.InetSocketAddress;
 import java.net.URI;
 import java.nio.charset.StandardCharsets;
 import java.util.Locale;
+import java.util.logging.Logger;
 
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
@@ -18,9 +19,11 @@ import org.apache.commons.lang3.StringUtils;
  */
 public class SocialNetworkServer {
 
+    static final Logger logger = Logger.getLogger(SocialNetworkServer.class.getName());
     public static final int DEFAULT_PORT = 8087;
     public static final String contextPath = "/sns";
     private final int port;
+    private final PostStore postStore;
     private HttpServer httpServer;
 
     public SocialNetworkServer () {
@@ -28,7 +31,12 @@ public class SocialNetworkServer {
     }
 
     public SocialNetworkServer(final int port) {
+        this(port, new InMemoryPostStore());
+    }
+    
+    public SocialNetworkServer(final int port, final PostStore postStore) {
         this.port = port;
+        this.postStore = postStore;
     }
 
     public static void main(final String... args) {
@@ -59,10 +67,11 @@ public class SocialNetworkServer {
         return urlPartPath.substring(0, index);
     }
 
-    static class MyHandler implements HttpHandler {
+    class MyHandler implements HttpHandler {
 
         private static final int OK = 200;
         private static final int ACCEPTED = 202;
+        private static final int BAD_REQUEST = 400;
 
         @SuppressWarnings("ImplicitNumericConversion")
         @Override
@@ -81,13 +90,24 @@ public class SocialNetworkServer {
                 final String requestMethod = httpExchange.getRequestMethod();
                 switch (StringUtils.substringBetween(urlPathPart, "/")) {
                     case "posting": {
-                        final String username = StringUtils.removeStart(urlPathPart, "/");
-                        responseStatusCode = ACCEPTED;
+                        final String urlSegmentTail = StringUtils.removeStart(urlPathPart, "/posting");
+                        logger.info("Posting!");
+                        final String username = StringUtils.removeStart(urlSegmentTail, "/");
+                        
+                        logger.info("username: " + username);
+                        if (StringUtils.isEmpty(username)) {
+                            responseStatusCode = BAD_REQUEST;
+                        } else {
+                            responseStatusCode = ACCEPTED;
+                            postStore.persist(new Post("", ""));
+                        } 
                         response = "";
                         break;
                     }
                     case null:
+                        logger.info("Null!");
                     default:
+                        logger.info("Default!");
                         final boolean isPostMessage = requestBody.contains("->");
                         if (isPostMessage) {
                             response = "";
@@ -95,17 +115,13 @@ public class SocialNetworkServer {
                         } else {
                             final String username = StringUtils.removeStart(urlPathPart, "/");
                             if ("alice".equals(username.toLowerCase(Locale.ROOT))) {
-
                                 response = "Alice - I love the weather today (5 minutes ago)";
                             } else {
-
                                 response = "";
                             }
                             responseStatusCode = OK;
                         }
                 }
-
-
 
                 httpExchange.sendResponseHeaders(responseStatusCode, response.length());
                 final OutputStream outputStream = httpExchange.getResponseBody();
