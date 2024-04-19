9663769096a3fa3fe5716ea1ef466778a8c9f868
Lars Erdmann <lars.erdmann@uni-greifswald.de>
2024-04-19 13:09:25 +0200

    "mob next [ci-skip]"

--- src/test/java/montyhallkata/MontyHallGameTest.java
+++ src/test/java/montyhallkata/MontyHallGameTest.java
@@ -2,6 +2,8 @@ package montyhallkata;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.assertj.core.api.BDDAssertions.then;
 import static org.mockito.ArgumentMatchers.anyInt;
@@ -40,11 +42,30 @@ class MontyHallGameTest {
         final boolean playerWins = montyHallGame.playerSwitchesSelection();
     }
     
-    @Test @DisplayName("test")
-    void test() {
+    @ParameterizedTest
+    @DisplayName("method: playerKeepsSelection")
+    @CsvSource({"0, true, false",
+                "1, true, true",
+                "0, false, true",
+                "1, false, true",
+    })
+    void test(int playerFirstSelection, boolean playerKeepsSelection, boolean expectedPrize) {
         RandomNumberGenerator randomNumberGenerator = mock(RandomNumberGenerator.class);
         final MontyHallGame game = new MontyHallGame(randomNumberGenerator);
         given(randomNumberGenerator.nextInt(anyInt())).willReturn(0);
-        game.playersFirstSelection(1);
+        game.playersFirstSelection(playerFirstSelection);
+
+        game.gameMasterOpensDoor();
+
+        boolean result;
+        if (playerKeepsSelection) {
+            result = game.playerKeepsSelection();
+        } else {
+            result = game.playerSwitchesSelection();
+        }
+
+        then(result).isEqualTo(expectedPrize);
     }
+
+
 }
