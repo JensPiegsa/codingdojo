bf0560662ad0f31673e03f072faf5f0847bc87de
Lars Erdmann <lars.erdmann@uni-greifswald.de>
2024-05-17 12:08:10 +0200

    new old kata
    Co-authored-by: Jacob <jacob.braeutigam@uni-greifswald.de>
    Co-authored-by: Jens Piegsa <piegsa@gmail.com>

---
+++ src/test/java/gildedrose2/GildedRoseTest.java
@@ -0,0 +1,19 @@
+package gildedrose2;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class GildedRoseTest {
+
+    @Test
+    void foo() {
+        Item[] items = new Item[] { new Item("foo", 0, 0) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals("fixme", app.items[0].name);
+    }
+
+}
