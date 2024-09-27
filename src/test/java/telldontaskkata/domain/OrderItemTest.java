459f32db881d1d2fd7026ba8a6a3c55deb2fecfc
Lars Erdmann <lars.erdmann@uni-greifswald.de>
2024-09-27 12:42:11 +0200

    "mob next [ci-skip]"

---
+++ src/test/java/telldontaskkata/domain/OrderItemTest.java
@@ -0,0 +1,22 @@
+package telldontaskkata.domain;
+
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class OrderItemTest {
+    @Test
+    void canCalculateTaxAmount() {
+        Category smallTax = new Category("smallTax", new BigDecimal(10));
+        Product product = new Product("some food", new BigDecimal(123), smallTax);
+
+        OrderItem item = new OrderItem();
+        item.setProduct(product);
+        item.setQuantity(2);
+
+        assertThat(item.getTaxedAmount()).isEqualTo(new BigDecimal("24.6"));
+    }
+}
--- src/test/java/telldontaskkata/domain/ProductTest.java
+++ src/test/java/telldontaskkata/domain/ProductTest.java
@@ -5,11 +5,5 @@ import org.junit.jupiter.api.Test;
 import java.math.BigDecimal;
 
 class ProductTest {
-    @Test
-    void canCalculateTaxAmount() {
-        Category smallTax = new Category("smallTax", new BigDecimal("0.1"));
-        Product product = new Product("some food", new BigDecimal(123), smallTax);
 
-
-    }
 }
