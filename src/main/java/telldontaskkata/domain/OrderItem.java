349d6a84ba4160664151883faba429f119880048
Lars Erdmann <lars.erdmann@uni-greifswald.de>
2024-09-27 12:08:20 +0200

    "mob next [ci-skip]"

--- src/main/java/telldontaskkata/domain/OrderItem.java
+++ src/main/java/telldontaskkata/domain/OrderItem.java
@@ -1,6 +1,7 @@
 package telldontaskkata.domain;
 
 import java.math.BigDecimal;
+import java.util.Objects;
 
 public class OrderItem {
     private Product product;
@@ -38,5 +39,18 @@ public class OrderItem {
 
     public void setTax(BigDecimal tax) {
         this.tax = tax;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        OrderItem orderItem = (OrderItem) o;
+        return quantity == orderItem.quantity && Objects.equals(product, orderItem.product) && Objects.equals(taxedAmount, orderItem.taxedAmount) && Objects.equals(tax, orderItem.tax);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(product, quantity, taxedAmount, tax);
     }
 }
--- src/main/java/telldontaskkata/domain/Product.java
+++ src/main/java/telldontaskkata/domain/Product.java
@@ -1,6 +1,7 @@
 package telldontaskkata.domain;
 
 import java.math.BigDecimal;
+import java.util.Objects;
 
 public class Product {
     private String name;
@@ -25,4 +26,16 @@ public class Product {
         return category;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Product product = (Product) o;
+        return Objects.equals(name, product.name) && Objects.equals(price, product.price) && Objects.equals(category, product.category);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, price, category);
+    }
 }
