08bf802a32bb6eb4ac40df1bbc1c78710711eb79
Lars Erdmann <lars.erdmann@uni-greifswald.de>
2026-06-12 12:08:50 +0200

    new kata: fizz buzz overengineering
    Co-authored-by: Jacob Bräutigam <jacob.braeutigam@uni-greifswald.de>
    Co-authored-by: Alexander Koglin

---
+++ src/main/java/fizzbuzz/overengineered/FizzBuzzOE.java
@@ -0,0 +1,18 @@
+package fizzbuzz.overengineered;
+
+public class FizzBuzzOE {
+
+	public String run(final int i) {
+
+		if (i % 3 == 0 && i % 5 == 0) {
+			return "FizzBuzz";
+		}
+		if (i % 3 == 0) {
+			return "Fizz";
+		}
+		if (i % 5 == 0) {
+			return "Buzz";
+		}
+		return String.valueOf(i);
+	}
+}
---
+++ src/test/java/fizzbuzz/FizzBuzzOETest.java
@@ -0,0 +1,34 @@
+package fizzbuzz;
+
+import fizzbuzz.overengineered.FizzBuzzOE;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.BDDAssertions.then;
+
+public class FizzBuzzOETest {
+
+    /**  Patterns:
+     *
+     *  1. AbstractFactory
+     *  2. Static Factory Method
+     *  3. Singleton
+     *  4. StringBuilder
+     *  5. Constants & Variables
+     *  6. Code Smells
+     *  7. Refactorings
+     *  8. L33t 5peak
+     *  9. numerology of digits
+     */
+
+    @Test @DisplayName("can start fizzbuzz.")
+    void canStartFizzbuzz() {
+        FizzBuzzOE fizzBuzz = new FizzBuzzOE();
+
+        String result = fizzBuzz.run(10);
+
+        then(result).isEqualTo("test");
+    }
+
+
+}
